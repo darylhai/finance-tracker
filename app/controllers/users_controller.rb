1d1674b8ffb6731c8ece3d21406625809bd7eb8b
Daryl Haines <daryl@southeastpixels.com>
2018-04-29 00:53:46 +0000

    Add friendship model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   def my_portfolio
-    @user = current_user
     @user_stocks = current_user.stocks
+     @user = current_user
   end
+  
+  def my_friends
+    
+  end
+  
+  
 end
